82597d014bc46eaf7ad10fd65e6a6b6582fdb899
safayet2218 <safayet2218@gmail.com>
2024-03-22 10:53:30 +0600

    update side bar

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { FaBars } from "react-icons/fa";
 
-function Header() {
+type SetIsCollapsedFn = (isCollapsed: boolean) => void;
+
+function Header({isCollapsed, setIsCollapsed}:{isCollapsed:Boolean, setIsCollapsed:SetIsCollapsedFn}) {
   return (
     <div className='w-full h-[60px] bg-white px-[20px] flex items-center justify-between'>
       <div className='flex gap-5 items-center'>
-        <span>
-        <FaBars size={20}/>
-        </span>
+        <div onClick={()=>{setIsCollapsed(!isCollapsed)}} className=' hover:cursor-pointer'>
+        <FaBars size={20} />
+        </div>
             Logo
       </div>
       <div>
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,19 +1,22 @@
+"use client";
 import Header from "@/Components/Header";
 import LeftMenu from "@/Components/LeftMenu";
 import React, {ReactNode} from "react";
+import { useState } from "react";
 
 type LayoutProps = {
     children: ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({children}) => {
+    const [isCollapsed, setIsCollapsed] = useState(true);
+
     return (
         <main>
-            
             <div className="w-screen h-screen  bg-gray-500 flex ">
-                <LeftMenu />
+                <LeftMenu isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
                 <div className=" w-full">
-                <Header />
+                <Header isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed}/>
                 {children}
                 </div>
             </div>
